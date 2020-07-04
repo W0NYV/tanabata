25a7b67156e3a429d8e717aeaa26f48e4a7b1d4a
Tamawo Nagano <nagano.tamawo77@gmail.com>
2020-07-05 01:09:14 +0900

    added p5.dom.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,14 @@
 let stars = [];
 function setup() {
+
+	input = createInput();
+  input.position(20, 65);
+	input.attribute('placeholder', '願いはなんじゃ');
+
+  button = createButton('願う');
+  button.position(input.x + input.width, 65);
+	button.mousePressed(send);
+
 	createCanvas(windowWidth, windowHeight);
 	for(let i = 0; i < width/2; i++) {
 		stars.push(new Star(random(width), random(height)));
@@ -14,4 +23,25 @@ function draw() {
 		}
 		stars[i].display();
 	}
+}
+
+
+function send() {
+  const wish = input.value();
+  input.value('');
+
+	var url = "https://script.google.com/macros/s/AKfycbzFIfTrPr8GZiz99nkn8cg3XHYxn72hOglzRAfiE7N9tUa6PSI/exec?name=WOのテスト&wish=" + wish + "&x=CCC&y=DDDD"; // リクエスト先URL
+	var request = new XMLHttpRequest();
+	request.open('GET', url);
+	request.onreadystatechange = function () {
+		if (request.readyState != 4) {
+     	// リクエスト中
+    } else if (request.status != 200) {
+      // 失敗
+    } else {
+      // 取得成功
+      // var result = request.responseText;
+    }
+	};
+	request.send(null);
 }
